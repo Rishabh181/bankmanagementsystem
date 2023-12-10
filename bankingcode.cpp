f872b960bdbab06ad0cac9471c293c76f5bef26d
Rishabh <rishabhpatidar181@gmail.com>
2023-12-10 18:02:49 +0530

    new bankingsystemproject is added

---
+++ bankingcode.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main(){
+    cout<<"code is working"<<endl;
+    cout<<"haa bhai mei kaam karne laga"<<endl;
+    cot<<"kaam karu tho bhi dikkat hai kya tuje" <<endl;
+}
